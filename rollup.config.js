61823746dd8a7317e2c5c26694d41959db7048c0
John James <john.usa.james@gmail.com>
2023-01-15 16:39:03 +0000

    fix(rollup): remove cjs config

--- rollup.config.js
+++ rollup.config.js
@@ -7,37 +7,26 @@ import terser from "@rollup/plugin-terser"
 
 import { defineConfig } from "rollup"
 import { visualizer } from "rollup-plugin-visualizer"
-import dts from "rollup-plugin-dts"
 
 import pkg from "./package.json" assert { type: "json" }
 
-export default defineConfig([
-  {
-    input: "src/index.ts",
-    output: [
-      { file: pkg.main, format: "cjs", sourcemap: true },
-      { file: pkg.module, format: "es", sourcemap: true },
-    ],
-    plugins: [
-      commonjs(),
-      babel({
-        exclude: "node_modules/**",
-        babelHelpers: "bundled",
-      }),
-      nodeResolve(),
-      typescript(),
-      terser(),
-      replace({
-        "process.env.NODE_ENV": JSON.stringify("development"),
-        preventAssignment: true,
-      }),
-      visualizer(),
-    ],
-    external: ["react"],
-  },
-  {
-    input: "dist/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [dts()],
-  },
-])
+export default defineConfig({
+  input: "src/index.ts",
+  output: { file: pkg.main, format: "es", sourcemap: true },
+  plugins: [
+    commonjs(),
+    babel({
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+    }),
+    nodeResolve(),
+    typescript(),
+    terser(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+      preventAssignment: true,
+    }),
+    visualizer(),
+  ],
+  external: ["react"],
+})
