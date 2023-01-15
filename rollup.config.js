0521e815d91154e85b5fe195361a48691bff41cc
John James <john.usa.james@gmail.com>
2023-01-15 15:09:50 +0000

    fix(rollup): output cjs

--- rollup.config.js
+++ rollup.config.js
@@ -4,18 +4,18 @@ import commonjs from "@rollup/plugin-commonjs"
 import replace from "@rollup/plugin-replace"
 import typescript from "@rollup/plugin-typescript"
 import terser from "@rollup/plugin-terser"
+import { defineConfig } from "rollup"
 
 import { visualizer } from "rollup-plugin-visualizer"
 
 import pkg from "./package.json"
 
-export default {
+export default defineConfig({
   input: "src/index.tsx",
-  output: {
-    file: pkg.main,
-    format: "esm",
-    sourcemap: true,
-  },
+  output: [
+    { file: pkg.main, format: "cjs", sourcemap: true },
+    { file: pkg.module, format: "es", sourcemap: true },
+  ],
   plugins: [
     commonjs(),
     babel({
@@ -32,4 +32,4 @@ export default {
     visualizer(),
   ],
   external: ["react", "react-dom"],
-}
+})
