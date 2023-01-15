9e22f417665ae46cd75f6cdc8e5d770dae641256
John James <john.usa.james@gmail.com>
2023-01-15 22:01:03 +0000

    build(rollup): remove babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,13 @@
-import babel from "@rollup/plugin-babel"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
-import replace from "@rollup/plugin-replace"
 import typescript from "@rollup/plugin-typescript"
 import terser from "@rollup/plugin-terser"
 
 import { defineConfig } from "rollup"
-import { visualizer } from "rollup-plugin-visualizer"
-
-import pkg from "./package.json" assert { type: "json" }
 
 export default defineConfig({
   input: "src/index.ts",
-  output: { file: pkg.main, format: "es", sourcemap: true },
-  plugins: [
-    commonjs(),
-    babel({
-      exclude: "node_modules/**",
-      babelHelpers: "bundled",
-    }),
-    nodeResolve(),
-    typescript(),
-    terser(),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("development"),
-      preventAssignment: true,
-    }),
-    visualizer(),
-  ],
+  output: { dir: "dist", format: "es", sourcemap: true },
+  plugins: [commonjs(), nodeResolve(), typescript(), terser()],
   external: ["react"],
 })
