d4b7b19f29dc549a9c0d4461c31c75cb6a6ef822
John James <john.usa.james@gmail.com>
2023-01-15 15:34:32 +0000

    fix(rollup): configure types bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,32 +4,40 @@ import commonjs from "@rollup/plugin-commonjs"
 import replace from "@rollup/plugin-replace"
 import typescript from "@rollup/plugin-typescript"
 import terser from "@rollup/plugin-terser"
+
 import { defineConfig } from "rollup"
-
 import { visualizer } from "rollup-plugin-visualizer"
+import dts from "rollup-plugin-dts"
 
-import pkg from "./package.json"
+import pkg from "./package.json" assert { type: "json" }
 
-export default defineConfig({
-  input: "src/index.tsx",
-  output: [
-    { file: pkg.main, format: "cjs", sourcemap: true },
-    { file: pkg.module, format: "es", sourcemap: true },
-  ],
-  plugins: [
-    commonjs(),
-    babel({
-      exclude: "node_modules/**",
-      babelHelpers: "bundled",
-    }),
-    nodeResolve(),
-    typescript(),
-    terser(),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("development"),
-      preventAssignment: true,
-    }),
-    visualizer(),
-  ],
-  external: ["react", "react-dom"],
-})
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    output: [
+      { file: pkg.main, format: "cjs", sourcemap: true },
+      { file: pkg.module, format: "es", sourcemap: true },
+    ],
+    plugins: [
+      commonjs(),
+      babel({
+        exclude: "node_modules/**",
+        babelHelpers: "bundled",
+      }),
+      nodeResolve(),
+      typescript(),
+      terser(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("development"),
+        preventAssignment: true,
+      }),
+      visualizer(),
+    ],
+    external: ["react"],
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+])
