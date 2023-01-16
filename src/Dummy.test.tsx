278dccd89d6f6d9323ffe552c04165e3bd24566a
John James <57100733+john-james-cko@users.noreply.github.com>
2023-01-16 11:10:14 +0000

    test(jest): setup dom matchers (#16)

--- src/Dummy.test.tsx
+++ src/Dummy.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from "@testing-library/react"
-import "@testing-library/jest-dom"
 
 import { Dummy } from "./Dummy"
 
 it("should render `title` and `children`", () => {
   render(
     <Dummy title="Hi!">
-      <p>I'm Dummy</p>
+      <p>I'm Dummy!</p>
     </Dummy>
   )
 
   expect(screen.getByText("Hi!")).toBeInTheDocument()
+  expect(screen.getByText("I'm Dummy!")).toBeInTheDocument()
 })
